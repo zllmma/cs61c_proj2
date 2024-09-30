c7810d9d9fd093513a66d5987bdb74d941c794f7
supersonic <zll040519@outlook.com>
2024-09-30 18:53:06 +0800

    Completed abs and relu

--- src/relu.s
+++ src/relu.s
@@ -14,25 +14,31 @@
 # ==============================================================================
 relu:
     # Prologue
-
-
+    # End prologue
+    li t0, 0
+    ble a1, x0, relu_error
+    
 loop_start:
+    bge t0, a1, loop_end
+    slli t1, t0, 2
+    add t3, a0, t1
+    
+    lw t2, 0(t3)
+    blt t2, x0, set_zero
+    j loop_continue
 
-
-
-
-
-
-
+set_zero:
+    sw x0 0(t3)
 
 loop_continue:
-
-
+    addi t0, t0, 1
+    j loop_start
 
 loop_end:
-
-
     # Epilogue
-
-
+    # End Epilogue
     jr ra
+    
+ relu_error:
+    li a0, 36
+    j exit
