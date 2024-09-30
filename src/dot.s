70f37ea69465c221c516a1fe9b78d2ac99688a2c
superonic <zll040519@outlook.com>
2024-10-01 01:02:05 +0800

    completed dots

--- src/dot.s
+++ src/dot.s
@@ -18,27 +18,60 @@
 #     this function terminates the program with error code 37
 # =======================================================
 dot:
-
-    # Prologue
-
-
+    ebreak
+    addi sp, sp, -12
+    sw s0, 0(sp)
+    sw s1, 4(sp)
+    sw s2, 8(sp)
+    
+    ble a2, x0, length_err
+    ble a3, x0, stride_err
+    ble a4, x0, stride_err
+    
+    mv s0, a0 # copy the address of arr0
+    mv s1, a1 # copy the address of arr1
+    li s2, 0 # number of elements used
+    li t0, 0 # the current index of arr0
+    li t1, 0 # the current index of arr1
+    li a0, 0 # result
+    
 loop_start:
-
-
-
-
-
-
-
-
-
-
-
-
+    bge s2, a2, loop_end
+    
+    slli t2, t0, 2
+    slli t3, t1, 2
+    add t2, s0, t2
+    add t3, s1, t3
+    
+    lw t4, 0(t2)
+    lw t5, 0(t3)
+    mul t6, t4, t5
+    add a0, a0, t6
+    
+    addi s2, s2, 1
+    add t0, t0, a3
+    add t1, t1, a4
+    j loop_start
+    
 loop_end:
-
-
-    # Epilogue
-
-
+    lw s0, 0(sp)
+    lw s1, 4(sp)
+    lw s2, 8(sp)
+    addi sp, sp, 12
     jr ra
+    
+length_err:
+    lw s0, 0(sp)
+    lw s1, 8(sp)
+    lw s2, 8(sp)
+    addi sp, sp, 12
+    li a0, 36
+    j exit
+
+stride_err:
+    lw s0, 0(sp)
+    lw s1, 4(sp)
+    lw s2, 8(sp)
+    addi sp, sp, 12
+    li a0 37
+    j exit
