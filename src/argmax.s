ecd60ae3d929857dc2d7f59460f5a9f836107808
supersonic <zll040519@outlook.com>
2024-09-30 20:47:54 +0800

    Completed argmax

--- src/argmax.s
+++ src/argmax.s
@@ -15,16 +15,44 @@
 #     this function terminates the program with error code 36
 # =================================================================
 argmax:
-    # Prologue
-
-
+    ebreak
+    addi sp, sp, -4
+    sw s0, 0(sp)
+    
+    ble a1, x0, arg_max_error
+    mv s0, a0 # copy the address of array
+    li t0, 0 # curr index 
+    li a0, 0 # max index
+    
 loop_start:
-
+    beq t0, a1, loop_end # if curr_index == length, end the loop
+    
+    slli t1, t0, 2
+    add t1, s0, t1
+    lw t2, 0(t1)
+    
+    slli t3, a0, 2
+    add t3, s0, t3
+    lw t4, 0(t3)
+    
+    bgt t2, t4, set_max
+    j loop_continue
+    
+set_max:
+    mv a0, t0
+    j loop_continue
 
 loop_continue:
-
+    addi t0, t0, 1
+    j loop_start
 
 loop_end:
-    # Epilogue
-
+    lw s0, 0(sp)
+    addi sp, sp, 4
     jr ra
+    
+arg_max_error:
+    lw s0, 0(sp)
+    addi sp, sp, 4
+    li a0, 36
+    j exit
